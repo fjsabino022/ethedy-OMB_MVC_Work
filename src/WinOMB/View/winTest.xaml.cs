18254c9a5e95fd8923e1ab79902f87567456ffad
ethedy <ethedy@gmail.com>
2015-06-02 16:45:48 -0300

    Agrego test window

---
+++ src/WinOMB/View/winTest.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using WindowsOMB.ViewModel;
+using Infraestructura;
+
+namespace WindowsOMB.View
+{
+  /// <summary>
+  /// Interaction logic for winTest.xaml
+  /// </summary>
+  public partial class winTest : Window
+  {
+    private TestViewModel _viewModel;
+
+    public winTest()
+    {
+      InitializeComponent();
+
+      _viewModel = new TestViewModel();
+      Context.Current.ServiceProvider.AddService(typeof(LoginService), new LoginService());
+    }
+
+    private void TestLoaded(object sender, RoutedEventArgs e)
+    {
+      _viewModel.TryLogin();
+    }
+  }
+}
---
+++ src/WinOMB/ViewModel/TestViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+using Infraestructura;
+using WindowsOMB.View;
+
+namespace WindowsOMB.ViewModel
+{
+  public class TestViewModel : INotifyPropertyChanged
+  {
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public TestViewModel()
+    {
+      
+    }
+
+    public void TryLogin()
+    {
+      var login = Context.Current.ServiceProvider.GetService(typeof(LoginService)) as LoginService;
+
+      login.Show();
+    }
+  }
+}
