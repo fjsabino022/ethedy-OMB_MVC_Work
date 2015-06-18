bf10581332390620de4ef8f5570334d055b4736f
ethedy <ethedy@gmail.com>
2015-06-18 15:48:36 -0300

    Actualizar DB

---
+++ dbscripts/create_tables.sql
@@ -0,0 +1,44 @@
+﻿
+
+create table Personas
+(
+  ID                      uniqueidentifier        not null    default(newid()) primary key,
+  Nombre                  varchar(100)            not null,
+  Apellido                varchar(50)             not null,
+  Direccion               varchar(250),
+  Localidad               varchar(100),
+  Provincia               varchar(50),
+  CodigoPostal            varchar(10),
+  CorreoElectronico       varchar(50),
+  Telefono                varchar(50),
+  FechaNacimiento         date                    not null
+)
+
+create table Perfiles
+(
+  ID_Perfil             int             identity not null primary key,
+  Nombre                varchar(30)     not null,
+  Descripcion           varchar(200)    not null
+)
+
+create table Usuarios
+(
+  Login                       varchar(20)           not null      primary key,
+  Password                    varchar(50)           not null,
+  FechaExpiracionPassword     date,
+  FechaLastLogin              smalldatetime,
+  MustChangePass              bit,
+  EnforceExpiration           bit,
+  EnforceStrong               bit,
+  Enabled                     bit                   not null default(1),
+  ID_Persona                  uniqueidentifier      not null,
+  constraint FK_Usuarios_Personas foreign key (ID_Persona) references Personas(ID)
+)
+
+create table Usuarios_Perfiles
+(
+  Login         varchar(20)       not null,
+  ID_Perfil     int               not null,
+  constraint FK_Ususarios_Perfiles_Usuarios foreign key (Login) references Usuarios(Login),
+  constraint FK_Ususarios_Perfiles_Perfiles foreign key (ID_Perfil) references Perfiles(ID_Perfil)
+)
---
+++ dbscripts/insert_data.sql
@@ -0,0 +1,42 @@
+﻿
+
+insert into Personas (ID, Nombre, Apellido, CorreoElectronico, FechaNacimiento) 
+  values (default, 'Homer', 'Simpson', 'hsimpson@pp.com', convert(date, '20/05/1968', 103))
+
+insert into Personas (ID, Nombre, Apellido, CorreoElectronico, FechaNacimiento) 
+  values (default, 'Abraham', 'Simpson', 'asimpson@pp.com', convert(date, '01/06/1923', 103))
+
+insert into Personas (ID, Nombre, Apellido, CorreoElectronico, FechaNacimiento) 
+  values (default, 'Montgomery', 'Burns', 'mburns@centralnuclear.com', convert(date, '13/04/1875', 103))
+
+insert into Personas (ID, Nombre, Apellido, CorreoElectronico, FechaNacimiento) 
+  values (default, 'Marge', 'Bouvier', 'mbouvier@pp.com', convert(date, '01/03/1971', 103))
+
+
+insert into Perfiles values ('SysAdmin', 'Administracion total del Sistema')
+insert into Perfiles values ('StockAdmin', 'Maneja el ingreso/egreso de productos al deposito')
+insert into Perfiles values ('PDV', 'Atencion al cliente, facturacion, caja')
+insert into Perfiles values ('AsistenciaCliente', 'Acceso a terminales de ayuda al cliente, busqueda de productos y precios')
+
+
+insert into Usuarios (Login, Password, ID_Persona)
+  values ('hsimpson', '123456', 'B9493449-D006-4270-B8EA-9CFE34DF7123')
+
+insert into Usuarios (Login, Password, ID_Persona)
+  values ('mburns', 'monty', '3008DEAE-F9F6-4BDB-88CB-904DE6A5348D')
+
+insert into Usuarios (Login, Password, ID_Persona)
+  values ('mbouvier', 'ringo', '3D1094EB-ED22-493A-87CD-7A78193F2B33')
+
+insert into Usuarios_Perfiles values ('hsimpson', 4)
+insert into Usuarios_Perfiles values ('mburns', 1)
+insert into Usuarios_Perfiles values ('mbouvier', 3)
+insert into Usuarios_Perfiles values ('mbouvier', 4)
+
+
+select * from Personas
+select * from Perfiles
+select * from Usuarios
+select * from Usuarios_Perfiles
+
+
