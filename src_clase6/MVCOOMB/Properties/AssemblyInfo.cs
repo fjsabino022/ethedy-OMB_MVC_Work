bd0aa0019fe6b99efb0b48ef705e90e694113e38
Enrique Thedy <ethedy@gmail.com>
2015-09-15 15:04:31 -0300

    Fuente final Clase 6

--- src_clase6/MVCOOMB/Properties/AssemblyInfo.cs
+++ src_clase6/MVCOOMB/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.15251.99")]
-[assembly: AssemblyFileVersion("1.0.15251.99")]
+[assembly: AssemblyVersion("1.0.15258.106")]
+[assembly: AssemblyFileVersion("1.0.15258.106")]
