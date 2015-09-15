bd04e5d40a63ba53e743232382e164d2792652e0
Enrique Thedy <ethedy@gmail.com>
2015-09-15 18:00:54 -0300

    Clase 7

--- src_clase7/MVCOOMB/Controllers/IngresosController.cs
+++ src_clase7/MVCOOMB/Controllers/IngresosController.cs
@@ -3,15 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Data;
+using Entidades;
 
 namespace MvcOMB.Controllers
 {
   public class IngresosController : Controller
   {
-    // GET: Ingresos
-    public ActionResult Index()
+    //  Action Method para un nuevo libro en blanco (retorna una vista con los campos vacios...)
+    public ActionResult NewLibro()
     {
-      return View();
+      Libro nuevoLibro = new Libro();
+
+      return View(nuevoLibro);
+    }
+
+    public ActionResult Agregar(Libro newLibro)
+    {
+      OMBContext ctx = DB.Contexto;
+
+      if (string.IsNullOrEmpty(newLibro.ISBN13))
+        ModelState.AddModelError("ISBN13", "El campo ISBN nuevo no puede dejarse vacio!!");
+
+      if (ModelState.IsValid)
+      {
+        try
+        {
+          ctx.Libros.Add(newLibro);
+          ctx.SaveChanges();
+        }
+        catch (Exception)
+        {
+          return new HttpUnauthorizedResult();
+        }
+        return View();
+      }
+      return View("NewLibro", newLibro);
     }
   }
 }
--- src_clase7/MVCOOMB/Properties/AssemblyInfo.cs
+++ src_clase7/MVCOOMB/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.15258.106")]
-[assembly: AssemblyFileVersion("1.0.15258.106")]
+[assembly: AssemblyVersion("1.0.15258.115")]
+[assembly: AssemblyFileVersion("1.0.15258.115")]
